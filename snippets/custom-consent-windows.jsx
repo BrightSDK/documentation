1afc68bbbd65ad77448f79174835eaec9f9c4d8f
alexanderg-brd <alexanderg@hola.org>
2026-07-30 12:22:04 +0300

    Replace ordered lists with unordered lists in consent text

--- snippets/custom-consent-windows.jsx
+++ snippets/custom-consent-windows.jsx
@@ -16,68 +16,15 @@ export const CustomConsentWin = ({ platform }) => (
 
     <p>Your custom consent screen must comply with the following requirements:</p>
 
-    <ol>
+    <ul>
       <li><strong>Mandatory Text -</strong> Your screen must include the following text exactly as written:</li>
-    </ol>
+    </ul>
 
-      <pre><code className="language-markdown">{`" To [Benefit to user], please allow Web Indexing by Bright Data to use your device's free resources and IP address to download public web data from the Internet.
-
-      Bright Data values your trust and takes every measure possible to protect your privacy and personal data.
-
-      Bright Data understands the security matters at stake in sharing your IP address and monitors all of its network traffic to ensure your safety.
-
-      Bright Data will only use your IP address for approved business-related use cases and never for unauthorized cases.
-
-      None of your personal information is accessed or collected except your IP address."`}</code></pre>
-
-    <ol start="2">
-      <li><strong>Additional Required Text</strong></li>
-    </ol>
-
-      <pre><code className="language-markdown">{`"Learn more about web indexing by Bright Data
-      Learn more about Bright Data’s Privacy Policy
-      Bright Data runs in the background even after closing the application.
-      Updates will be automatically downloaded by your device from time to time and will be subject to the EULA (End User Level Agreement)"`}</code></pre>
+    <pre><code className="language-markdown">{`"{benefit}, please allow web indexing by Bright Data to occasionally use your device’s free resources and IP address to download public web data from the Internet. Bright Data values your trust and takes every measure possible to protect your privacy and personal data. Bright Data understands the security matters at stake in sharing your IP address and monitors all of its network traffic to ensure your safety. Bright Data will only use your IP address for approved business-related use cases and never for unauthorized cases. None of your personal information is accessed or collected except your IP address."`}</code></pre>
 
     <Note>
-      <strong>Important:</strong> "<a href="https://bright-sdk.com/users#learn-more-about-bright-sdk-web-indexing">Learn more</a>", "<a href="https://bright-sdk.com/privacy-policy">Privacy Policy</a>" and "<a href="https://bright-sdk.com/eula">EULA (End User Level Agreement)</a>" should redirect to the relevant links.
+      <strong>Important:</strong> "<a href="https://bright-sdk.com/users#learn-more-about-bright-sdk-web-indexing">Learn more</a>", "<a href="https://bright-sdk.com/privacy-policy">Privacy Policy</a>", "<a href="https://bright-sdk.com/eula">EULA (End User Level Agreement)</a>", and "<a href="https://bright-sdk.com/ethical-usage-of-residential-proxies">use cases</a>" should redirect to the relevant links.
     </Note>
-
-    <ol start="3">
-      <li><strong>Interactive Popups -</strong> Clicking on "<em>will only use</em>" → Opens popup:</li>
-    </ol>
-
-      <pre><code className="language-markdown">{`"Your IP address WILL be used for cases such as: 
-      Support academic research 
-      Help brands track sites selling fake products 
-      Collect public web data like product prices and reviews 
-      Aggregate travel information like flights and hotel prices "`}</code></pre>
-
-    <p>Example:</p>
-
-    <img
-      src="/snippets/image/custom-consent-windows-1.png"
-      alt="Alt text"
-      width="400"
-    />
-
-    <p>Clicking on "never" → Opens popup:</p>
-
-    <pre><code className="language-markdown">{`"Your IP address will NEVER be misused: 
-
-Never to steal, encrypt, or delete sensitive data 
-Never to alter or hijack core computing functions 
-Never to monitor users' computer activity 
-Never to access illegal sites (dark web, porn, etc.) 
-Never to perform DDOS attacks "`}</code></pre>
-
-    <p>Example:</p>
-
-    <img
-      src="/snippets/image/custom-consent-windows-2.png"
-      alt="Alt text"
-      width="400"
-    />
 
     <p><strong>Integration Code:</strong></p>
 
