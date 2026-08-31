276a4f4cde0a913ddc958122d329103fc81eb5b6
alexanderg-brd <alexanderg@hola.org>
2026-08-31 15:45:26 +0300

    Change image sources in custom-consent-windows.jsx
    Updated image source paths for custom consent windows.

--- snippets/custom-consent-windows.jsx
+++ snippets/custom-consent-windows.jsx
@@ -7,7 +7,7 @@ export const CustomConsentWin = ({ platform }) => (
     <p>This is what it looks like on the default Bright SDK screen:</p>
 
     <img
-      src="/images/snippets/default_consent_screen.png"
+      src="/_mintlify/image/brightdatasdk/images/snippets/default_consent_screen.png"
       alt="Alt text"
       width="400"
     />
@@ -52,13 +52,13 @@ Read Bright Data's [Privacy Policy] and [End User License Agreement].`}</code></
         <p>Example Custom Screens:</p>
 
         <img
-          src="/images/snippets/custom-consent-windows-3.png"
+          src="/_mintlify/image/brightdatasdk/images/snippets/custom-consent-windows-3.png"
           alt="Alt text"
           width="400"
         />
         <br />
         <img
-          src="/images/snippets/custom-consent-windows-4.png"
+          src="/_mintlify/image/brightdatasdk/images/snippets/custom-consent-windows-4.png"
           alt="Alt text"
           width="400"
         />
