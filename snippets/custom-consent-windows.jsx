4e432780c53d9f388f1f3530f2ea6d0043a7841e
alexanderg-brd <alexanderg@hola.org>
2026-09-01 17:08:25 +0300

    Revise custom consent window content
    Updated consent text and added additional required links for web indexing.

--- snippets/custom-consent-windows.jsx
+++ snippets/custom-consent-windows.jsx
@@ -20,19 +20,59 @@ export const CustomConsentWin = ({ platform }) => (
       <li><strong>Mandatory Text -</strong> Your screen must include the following text exactly as written:</li>
     </ul>
 
-    <pre><code className="language-markdown">{`To Get [Benefit to user], please allow Web Indexing by [Bright Data] to use your device's free resources and IP address to download public web data from the Internet while you are using the app.
-    
-None of your personal information is collected, except your IP address. Bright Data does not track you.
+    <pre><code className="language-markdown">{`To [Benefit to user], please allow Web Indexing by Bright Data to use your device's free resources and IP address to download public web data from the Internet. 
 
-Bright Data will only use your IP address for approved business-related [use cases] and never for unauthorized cases.
+Bright Data values your trust and takes every measure possible to protect your privacy and personal data. Bright Data does not track you.
 
-You may opt out any time by going to settings and switching off "Web Indexing".
+Bright Data understands the security matters at stake in sharing your IP address and monitors all of its network traffic to ensure your safety. 
 
-Read Bright Data's [Privacy Policy] and [End User License Agreement].`}</code></pre>
+Bright Data will only use your IP address for approved business-related use cases and never for unauthorized cases. 
 
-    <Note>
-      <strong>Important:</strong> "<a href="https://bright-sdk.com/users#learn-more-about-bright-sdk-web-indexing">Learn more</a>", "<a href="https://bright-sdk.com/privacy-policy">Privacy Policy</a>", "<a href="https://bright-sdk.com/eula">EULA (End User Level Agreement)</a>", and "<a href="https://bright-sdk.com/ethical-usage-of-residential-proxies">use cases</a>" should redirect to the relevant links.
-    </Note>
+None of your personal information is accessed or collected except your IP address.`}</code></pre>
+
+    <ul>
+      <li><strong>Additional Required Text:</strong></li>
+    </ul>
+
+    <pre><code className="language-markdown">{`Learn more about web indexing by Bright Data
+
+Learn more about Bright Data’s Privacy Policy
+
+Bright Data runs in the background even after closing the application.
+
+Updates will be automatically downloaded by your device from time to time and will be subject to the EULA (End User Level Agreement)`}</code></pre>
+
+    <p><strong>Required links:</strong></p>
+    <table>
+      <thead>
+        <tr>
+          <th>Text</th>
+          <th>URL</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Web Indexing</td>
+          <td><a href="https://bright-sdk.com/users#learn-more-about-bright-sdk-web-indexing">https://bright-sdk.com/users#learn-more-about-bright-sdk-web-indexing</a></td>
+        </tr>
+        <tr>
+          <td>Bright Data</td>
+          <td><a href="https://brightdata.com/">https://brightdata.com/a></td>
+        </tr>
+        <tr>
+          <td>Privacy Policy</td>
+          <td><a href="https://bright-sdk.com/privacy-policy">https://bright-sdk.com/privacy-policy</a></td>
+        </tr>
+        <tr>
+          <td>EULA (End User Level Agreement)</td>
+          <td><a href="https://bright-sdk.com/eula">https://bright-sdk.com/eula</a></td>
+        </tr>
+        <tr>
+          <td>Use Cases</td>
+          <td><a href="https://bright-sdk.com/ethical-usage-of-residential-proxies">https://bright-sdk.com/ethical-usage-of-residential-proxies</a></td>
+        </tr>
+      </tbody>
+    </table>
 
     <p><strong>Integration Code:</strong></p>
 
